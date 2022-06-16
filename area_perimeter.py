2cf9141cdb648dd43bd9e1d43bd680ed73a77360
mohammedal-ess <99287125+mohammedal-ess@users.noreply.github.com>
2022-06-16 14:25:18 -0400

    Create area_perimeter.py

---
+++ area_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: mohammed.al-essawi
+# Created on: fep 2022
+# This program calculates the area and perimeter of a rectangle
+#    with dimensions 5cm x 3cm
+
+def main():
+    print("If rectangle has the dimensions")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {} cm^2".format(3*5))
+    print("Perimeter is {} cm".format(2*(3+5)))
+
+
+if __name__ == "__main__":
+    main()
